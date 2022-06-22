b41780c5f9651b8ab9bd9937c8d978446ed0789b
Sumanraj-10 <107987037+Sumanraj-10@users.noreply.github.com>
2022-06-22 13:29:43 +0530

    Add files via upload

---
+++ use.pl
@@ -0,0 +1,10 @@
+print "enter your name\n";
+$name = <STDIN>;
+print "Entered name is : ". $name;
+
+print "Enter tamil marks : \n";
+$tamil = <STDIN>;
+print "Enter history marks : \n";
+$history = <STDIN>;
+print "Enter english marks : \n";
+$english = <STDIN>;
